3cae944c95ab8e52c4971f39592f546aec37bdeb
Ethan McPherson <emcpherson121@gmail.com>
2024-08-20 14:37:16 +1000

    Make prettier better

---
+++ src/components/mine/AuthUserCard.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button"
+import { auth, signIn, signOut } from "@/auth"
+import Image from "next/image"
+
+export const AuthUserCard = async () => {
+  const session = await auth()
+  if (!session?.user)
+    return (
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      >
+        <button type="submit">Signin with GitHub</button>
+      </form>
+    )
+  return (
+    <div className="flex flex-row items-center gap-20">
+      <div className="flex flex-row items-center gap-8">
+        {session.user.image && (
+          <Image
+            width={"100"}
+            height={"100"}
+            alt="Profile Image"
+            src={session.user.image}
+          />
+        )}
+        <div className="flex flex-col gap-2">
+          <div>{session.user.name}</div>
+          <div>{session.user.email}</div>
+        </div>
+      </div>
+      <form
+        action={async () => {
+          "use server"
+          await signOut()
+        }}
+      >
+        <Button type="submit">Signout</Button>
+      </form>
+    </div>
+  )
+}
